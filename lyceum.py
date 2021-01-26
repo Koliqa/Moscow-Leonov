6c5390df1a8eb5c133ea3596c17099e331672108
Koliqa <kolyaleonof@yandex.ru>
2021-01-26 21:17:15 +0300

    0 commit

---
+++ lyceum.py
@@ -0,0 +1,6 @@
+is_it_my_first_git_repo = False
+but_should_i_write_it_anyway = True
+if is_it_my_first_git_repo:
+    print('My first git-repo')
+elif but_should_i_write_it_anyway:
+    print('My first git-repo')
